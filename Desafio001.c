984b8a27676e75ac27ad44c7ad4a4dd9c36385a9
Vitor Costa <vitor.costafelippe@gmail.com>
2022-08-25 10:54:42 -0300

    Dev validação da escolha

--- Desafio001.c
+++ Desafio001.c
@@ -2,53 +2,52 @@
 #include <stdlib.h> 
 
 int tipoMatriz();
-void matrizOrdem();
+int validaNum(int escolha);
 
-int main() {
 
-    int tipoM = tipoMatriz();
+int main() 
+{
 
-    if(tipoM == 0) {
-        printf("\n\n\tMatriz escolhida: [Matriz Quadrada].");
-    } else {
-        
-    }
+    int escolha = tipoMatriz(); //Variável 
+
+    printf("Escolha %d", escolha);
+    
 
     getchar();
     return 0;
 
 }
 
-int tipoMatriz(){
+int tipoMatriz() //Captura o tipo da matriz.
+{ 
     
     int escolha;
-
-    printf("\n\tEscolha se sera matriz quadrada[ixi] ou matriz A[lxc]. ");
-    printf("\n\n\t\tPara 0 - Q[ixi]\t| 1 - A[lxc]: ");
-    scanf("%d%*c", &escolha);
-    
-    
-        while(escolha > 1) {
-
-            printf("\n\tEscolha invalida, Digite: ");
-            printf("\n\n\t\t {0} - Q[ixi]\t| {1} - A[lxc]: ");
-            scanf("%d%*c", &escolha);
-            
-            while(escolha < 0) {
-
-                printf("\n\tEscolha invalida, Digite: ");
-                printf("\n\n\t\t {0} - Q[ixi]\t| {1} - A[lxc]: ");
-                scanf("%d%*c", &escolha);
-    
-            }
-
-        }
+        printf("\n\n\n\tEscolha se sera matriz quadrada[ixi] ou matriz A[lxc]. ");
+        printf("\n\n\t\tPara 0 - Q[ixi]\t| 1 - A[lxc]: ");
+        scanf("%d%*c", &escolha);
+        escolha = validaNum(escolha);
 
     return escolha;
 }
 
+int validaNum(int escolha) //Restringe a escolha -- ERRO com caractéres.
+{
+    int escolhaValida = escolha;
+    int erroNum = 0;
 
+    do{
+            if (escolhaValida != 0 && escolhaValida != 1){
+              
+                    erroNum = 9;
+                    printf("\n\n\n\tEscolha se sera matriz quadrada[ixi] ou matriz A[lxc]. ");
+                    printf("\n\n\t\tPara 0 - Q[ixi]\t| 1 - A[lxc]: ");
+                    scanf("%d%*c", &escolhaValida);
+            } else {
+                erroNum =0;
+            }
+        }while(erroNum != 0);
 
-void matrizOrdem() {
-
+     escolhaValida;
+    
+    return escolhaValida; 
 }
